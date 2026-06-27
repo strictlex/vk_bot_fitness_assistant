bbe33de0ea14198f2a8c082e8053899199afeceb
strictlex <pislegin2688@gmail.com>
2026-06-27 15:52:56 +0400

    del hardcod in config

--- config.py
+++ config.py
@@ -8,6 +8,19 @@ if not VK_TOKEN:
     raise ValueError("VK_TOKEN не задан в .env")
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 SECRET_FILE = 'credentials.json'
-TIMEZONE_OFFSET = int(os.getenv('TIMEZONE_OFFSET', 4))
-TRAINER_VK_ID = int(os.getenv('TRAINER_VK_ID', 15705227))
-GROUP_ID = int(os.getenv('GROUP_ID', 239622096))
+
+timzone_offset = os.getenv('TIMEZONE_OFFSET')
+if not timzone_offset:
+    raise ValueError("TIMEZONE_OFFSET не задан в .env")
+TIMEZONE_OFFSET = int(timzone_offset)
+
+trainer_vk = os.getenv('TRAINER_VK_ID')
+if not trainer_vk:
+    raise ValueError("TRAINER_VK_ID не задан в .env")
+TRAINER_VK_ID = int(trainer_vk)
+
+group_id = os.getenv('GROUP_ID')
+if not group_id:
+    raise ValueError("GROUP_ID не задан в .env")
+GROUP_ID = int(group_id)
+
