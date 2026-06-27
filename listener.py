82a003007589584b511b1e9edbfaf3e1e060db54
strictlex <pislegin2688@gmail.com>
2026-06-27 18:03:47 +0400

    correct listener

--- listener.py
+++ listener.py
@@ -42,10 +42,14 @@ def main(file):
                                     message=f"От клиента {name}(vk.com/id{from_id}):)",
                                     random_id=get_random_id()
                                     )
-                                vk.messages.forward_messages(
-                                    peer_id = from_id,
-                                    message_ids = [event.obj.message['id']], # type: ignore
-                                    user_id = config.TRAINER_VK_ID )
+                                vk.messages.send(
+                                    user_id=config.TRAINER_VK_ID,
+                                    random_id=get_random_id(),
+                                    forward={
+                                        'peer_id': from_id,
+                                        'message_ids': [event.obj.message['id']] # type: ignore
+                                    }
+                                )
                                 logging.info(f"Переслано сообщение от клиента {name} тренеру")
                                 found = True
                                 break
